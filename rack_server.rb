831c0e3ae91df02d6b9eb1a8ebf4fd7a51e2c87a
yohei.hokari <yohei.hokari@gmail.com>
2025-07-17 07:40:27 +0900

    wip: rack_server

--- rack_server.rb
+++ rack_server.rb
@@ -1,7 +1,11 @@
 require 'socket'
-require_relative "./app"
 
-app = App.new
+app_file = File.basename(ARGV[0], '.rb')
+require_relative "./#{app_file}"
+
+app_class = Object.const_get(File.basename(app_file, '.rb').capitalize)
+app = app_class.new
+
 server = TCPServer.new(3000)
 
 puts "サーバー起動中！！！！"
@@ -9,6 +13,14 @@ puts "サーバー起動中！！！！"
 loop do
   socket = server.accept
   request = socket.gets
+
+  puts "ソケット作成、リクエスト受付！！！"
+  if request.nil?
+    socket.close
+    puts "ソケット緊急終了！！！"
+    next
+  end
+
   env = {}
   env["REQUEST_METHOD"], env["PATH_INFO"], _ = request.split
   status, headers, body = app.call(env)
@@ -20,10 +32,11 @@ loop do
   header_lines = headers.map { |k, v| "#{k}: #{v}" }.join("\r\n")
   res = "HTTP/1.1 #{status} #{reason}\r\n" \
         "#{header_lines}\r\n" \
-        "Content-Length: #{body.bytesize}\r\n" \
+        "Content-Length: #{body.join.bytesize}\r\n" \
         "\r\n" \
         "#{body.join}"
 
   socket.print res
   socket.close
+  puts "サーバー終了！！！！"
 end
